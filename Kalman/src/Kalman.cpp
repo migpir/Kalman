4d67c1f70feaf30053d4e1fdb8c88b175513e47b
migpir <tigerito123@yahoo.com>
2017-05-18 20:19:14 -0400

    Updates to Kalman.

--- Kalman/src/Kalman.cpp
+++ Kalman/src/Kalman.cpp
@@ -1,13 +1,25 @@
 //============================================================================
 // Name        : Kalman.cpp
 // Author      : Miguel H. Peralta
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Version     : 1.0
+// Copyright   : This software may be distributed and modified under the terms
+//             : GNU General Public License as published by the Free Software
+//             : Foundation.
+// Description : One dimensional Kalman filter. in C++.
+//             : May 18th, 2017
 //============================================================================
+
+#ifndef _Kalman_h_
+#define _Kalman_h_
+
 
 #include <iostream>
 using namespace std;
+
+class Kalman {
+public:
+	kalman();
+};
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
